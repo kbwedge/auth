058739728114ab9f2d19f5bf4a4e2a3a5055b7f6
kbwedge <kevin.wedge@kellogg.northwestern.edu>
2022-02-22 16:42:12 +0000

    Added logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,6 +26,11 @@ class SessionsController < ApplicationController
             redirect_to"/sessions/new"
         end 
 
+        def destroy
+            session["user_id"] = nil
+            flash[:notice] ="You have been logged out"
+            redirect_to "/sessions/new"
+        end 
 
     end
 
